d891a1e4695d62c572b606b7d55aef2e40984e01
jtracy3 <joshtracy3@gmail.com>
2021-10-24 12:53:31 -0600

    added a few more endpoints. updated constants

--- nfl-api/espn/client.py
+++ nfl-api/espn/client.py
@@ -1,10 +1,10 @@
 import datetime as dt
 import requests
-from typing import Match, Union
+from typing import Union
 from collections import OrderedDict
 import re
 
-# from .constants import ALT_NAMES
+from .constants import TEAM_IDS
 
 class NFLClient:
 
@@ -24,6 +24,8 @@ class NFLClient:
     # Site endpoints ------------------------------
     SCOREBOARD_ENDPOINT = "/apis/site/{}/sports/football/nfl/scoreboard"
     SUMMARY_ENDPOINT = "/apis/site/{}/sports/football/nfl/summary"
+    TEAM_SCHEDULE_ENDPOINT = "/apis/site/{}/sports/football/nfl/teams/{}/schedule"
+    TEAMS_ENDPOINT = "/apis/site/{}/sports/football/nfl/teams"
 
     # Dates are in UTC
     DATE_FORMAT = "%Y-%m-%dT%H:%MZ"
@@ -39,9 +41,22 @@ class NFLClient:
         end = dt.datetime.strptime(r["endDate"], self.DATE_FORMAT).strftime("%Y%m%d")
         return start, end
 
-    # TODO: add get function that gets and returns as JSON with status code
-    def _get(url, params):
-        pass
+    @staticmethod
+    def _check_two_city_team(team):
+        if team.lower() == "los angeles" or team.lower() == "new york":
+            raise Exception(f"""
+                There a multiple teams with {team} in the name. Specify the team, e.g. Los Angeles Chargers
+                """)
+
+    @staticmethod
+    def _match_team_name(team_input, team_check):
+        team_input_l, team_check_l = team_input.lower(), team_check.lower()
+        return team_input_l in team_check_l or team_input_l in re.split('\W', team_check_l)
+
+    @staticmethod
+    def _match_team_abbr(abbr_input, abbr_check):
+        abbr_input_l, abbr_check_l = abbr_input.lower(), abbr_check.lower()
+        return abbr_input_l in re.split('\W+', abbr_check_l)
 
     def get_week_games(self, season: Union[int, str], week: Union[int, str]):
         """
@@ -59,10 +74,10 @@ class NFLClient:
         for event in events:
             games.append(
                 OrderedDict(
-                    id=event.get("id"),
+                    id=event["id"],
                     dateTime=self._convert_datetime_format(event.get("date"), self.DATE_FORMAT, self.CONVERT_DATE_FORMAT),
-                    name=event.get("name"),
-                    shortName=event.get("shortName"),
+                    name=event["name"],
+                    shortName=event["shortName"],
                     week=week
                 )
             )
@@ -75,13 +90,13 @@ class NFLClient:
 
         url = self.API_ESPN_SITE + self.SUMMARY_ENDPOINT.format(self.SITE_VERSION)
         params = {"event": game_id}
-        r = self.session.get(url, params=params)
-        r_json = r.json()
         
-        team_boxscores = r_json["boxscore"]["teams"]
+        r = self.session.get(url, params=params)
+        
+        team_boxscores = r.json()["boxscore"]["teams"]
         teams = []
         for team in team_boxscores:
-            record = OrderedDict(team_id=team["team"].get("id"))
+            record = OrderedDict(gameId=game_id, teamId=team["team"]["id"])
             while team["statistics"]:
                 stat = team["statistics"].pop(0)
                 record |= OrderedDict({stat["name"]: stat["displayValue"]})
@@ -89,7 +104,7 @@ class NFLClient:
 
         return teams
 
-    def get_gameid(self, date: Union[dt.date, str], team: str):
+    def get_game_id(self, date: Union[dt.date, str], team: str):
         """
         Get game id for team on a given date. For the team parameter
         either send the full name, partial name or team abbreviation
@@ -97,21 +112,23 @@ class NFLClient:
         date format "%Y%m%d"
         """
 
+        self._check_two_city_team(team)
+
         # TODO: maybe add help for datetime
         if type(date) is dt.date:
             date = date.strftime("%Y%m%d")
 
         url = self.API_ESPN_SITE + self.SCOREBOARD_ENDPOINT.format(self.SITE_VERSION)
         r = self.session.get(url, params={"dates": date})
+        
         events = r.json()["events"]
-
         unmatched = True
         while unmatched and events:
             event = events.pop()
             # if full name, e.g. Los Angeles Chargers check full name else check parts, i.e. Los Angeles OR Chargers
             name, abbr = event["name"], event["shortName"]
-            match_on_name = team.lower() in name.lower() or team.lower() in re.split('\W', name.lower())
-            match_on_abbr = team.lower() in re.split('\W+', abbr.lower())
+            match_on_name = self._match_team_name(team, name)
+            match_on_abbr = self._match_team_abbr(team, abbr)
             if match_on_name | match_on_abbr:
                 game_id = event["id"]
                 unmatched = False
@@ -121,6 +138,85 @@ class NFLClient:
 
         return game_id
         
+    def get_team_id(self, team_name: str):
+        """
+        Get team id for a team based on the team name/abbreviation
+        """
+        self._check_two_city_team()
+        
+        team_name_l = team_name.lower()
+        for k, v in TEAM_IDS.items():
+            if team_name_l in v:
+                return k
+        return print(f"{team_name} id not found")
+
+    def get_team_schedule(
+        self, team_id: Union[int, str], season: Union[int, str], season_type: Union[int, str]=2
+        ):
+
+        url = self.API_ESPN_SITE + self.TEAM_SCHEDULE_ENDPOINT.format(self.SITE_VERSION, team_id)
+        params = {"season": season, "seasontype": season_type}
+        r = self.session.get(url, params=params)
+        
+        events = r.json()["events"]
+        schedule = []
+        for event in events:
+            competitors = event["competitions"][0]["competitors"]
+            record = OrderedDict(
+                gameId=event["id"],
+                dateTime=self._convert_datetime_format(event["date"], self.DATE_FORMAT, self.CONVERT_DATE_FORMAT),
+                name=event["name"],
+                shortName=event["shortName"],
+                homeTeam=self._get_home_away(competitors)["home"]["id"],
+                homeTeamScore=self._get_home_away(competitors)["home"]["score"],
+                awayTeam=self._get_home_away(competitors)["away"]["id"],
+                awayTeamScore=self._get_home_away(competitors)["away"]["score"],
+                season=event["season"]["year"],
+                seasonType=event["seasonType"]["id"],
+                seasonTypeName=event["seasonType"]["name"],
+                week=event["week"]["number"]
+            )
+            schedule.append(record)
+        return schedule
+
+    def get_teams(self):
+        """
+        Only includes teams for current season. For example, you won't find the San Diego
+        Chargers nor the St. Louis Rams.
+        """
+        url = self.API_ESPN_SITE + self.TEAMS_ENDPOINT.format(self.SITE_VERSION)
+        r = self.session.get(url)
+        teams = r.json()["sports"][0]["leagues"][0]["teams"]
+        
+        teams_list = []
+        for team in teams:
+            team_ = team["team"]
+            record = OrderedDict(
+                id=team_["id"],
+                slug=team_["slug"],
+                location=team_["location"],
+                name=team_["name"],
+                nickname=team_["nickname"],
+                abbreviation=team_["abbreviation"],
+                displayName=team_["displayName"],
+                shortDisplayName=team_["shortDisplayName"]
+            )
+            teams_list.append(record)
+        
+        return teams_list
+
+    @staticmethod
+    def _get_home_away(response: dict) -> dict:
+        home_away = {}
+        for team in response:
+            home_away.update({
+                team["homeAway"]: {
+                    "id": team["id"],
+                    "score": team["score"]["value"]
+                }
+            })
+        return home_away
+            
 
     @staticmethod
     def _convert_datetime_format(
--- nfl-api/main.py
+++ nfl-api/main.py
@@ -9,4 +9,7 @@ from pprint import pprint
 nfl_client = NFLClient()
 # pprint(nfl.get_week_games(season=2021, week=1))
 # pprint(nfl_client.get_game_details(game_id="401326423"))
-pprint(nfl_client.get_gameid(date="20211017", team="Oakland"))
+# pprint(nfl_client.get_game_id(date="20211024", team="San Francisco"))
+# pprint(nfl_client.get_team_id("LAC"))
+# pprint(nfl_client.get_team_schedule(team_id="3", season=2019, season_type=2))
+pprint(nfl_client.get_teams())
